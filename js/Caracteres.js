83c988027668d20982972b5c299a934a89966005
LuisLuna24 <eduarlun4@gmail.com>
2023-10-05 09:49:31 -0600

    Agerege Login

---
+++ js/Caracteres.js
@@ -0,0 +1,51 @@
+var inputNumero = document.getElementById("RTelefono");
+inputNumero.addEventListener("input", function () {
+  var valor = inputNumero.value;
+  valor = valor.replace(/[^0-9]/g, "");
+  if (valor.length > 10) {
+    valor = valor.slice(0, 10);
+  }
+
+  inputNumero.value = valor;
+
+});
+
+
+function mostrarContraseña() {
+  var campoPassword = document.getElementById("Contraseña");
+  var eye=document.getElementById("Img_eye");
+  if(campoPassword.type=="password"){
+    campoPassword.type = "text";
+    eye.src="img/eyen.png";
+  }else if(campoPassword.type=="text"){
+    campoPassword.type = "password";
+    eye.src="img/eye.png";
+  }
+  
+}
+
+function mostrarContraseña1() {
+  var campoPassword = document.getElementById("Contraseña1");
+  var eye=document.getElementById("Img_eye1");
+  if(campoPassword.type=="password"){
+    campoPassword.type = "text";
+    eye.src="img/eyen.png";
+  }else if(campoPassword.type=="text"){
+    campoPassword.type = "password";
+    eye.src="img/eye.png";
+  }
+  
+}
+
+function mostrarContraseña2() {
+  var campoPassword = document.getElementById("Contraseña2");
+  var eye=document.getElementById("Img_eye2");
+  if(campoPassword.type=="password"){
+    campoPassword.type = "text";
+    eye.src="img/eyen.png";
+  }else if(campoPassword.type=="text"){
+    campoPassword.type = "password";
+    eye.src="img/eye.png";
+  }
+  
+}
